8841d33fd340e38a3a43bf23d65deed8e932aa70
rkjeeva489 <rjeeva1706@gmail.com>
2024-09-23 03:37:43 -0700

    error handling

--- Module/admin.js
+++ Module/admin.js
@@ -17,18 +17,18 @@ const Admin ={
                 username: req.body.username,
                 password: req.body.password
             }
-        // const id_psw = await bcrypt.hashSync(id.password,10);  
-        // console.log(id_psw);
+                // const id_psw = await bcrypt.hashSync(id.password,10);  
+                // console.log(id_psw);
             
             const result =await collection.find();
-            // console.log(result[0]['username'])
-            // console.log(result[0]['password'])
+                 // console.log(result[0]['username'])
+                // console.log(result[0]['password'])
             if(id.username==result[0]['username']&&id.password==result[0]['password']){
 
                 res.status(200).send({'Status':'Success',"result":result})
             }
             else{
-                    res.status(400).send({"Error":"Invalid Username & password........."})
+                res.status(400).send({"Error":"Invalid Username & password........."})
             }
 
         } catch (error) {
--- Module/student.js
+++ Module/student.js
@@ -38,43 +38,43 @@ const student ={
             } else {
                 res.status(500).json({
                 error: 'Internal Server Error'});
-            }}
+        }}
         },
     login:async(req,res)=>{
 
             try {
                 
-                    const userid = req.body.loginid;
-                    const  userpassword = req.body.loginpsw;
+                const userid = req.body.loginid;
+                const  userpassword = req.body.loginpsw;
                 const sessionId =uuid();
-                //console.log(login_id.userid);
+
+                        //user data Get methods.............
                 const result =await studentBio.findOne({"std_id" : userid});
 
-                //console.log(sessionId);
+                        //password comparesync methods.............
 
                 const psw = bcrypt.compareSync(userpassword,result.password)
 
-                //console.log(psw);
-                    if(!psw){
-                        res.status(400).send(`password Don't Match............`);
-                     }
-                    else{
-                        let token_id =  jwt.sign({ userid, userpassword},ACCESS_TOKEN,{expiresIn:'1d'});
+                if(!psw){
+                    res.status(400).send(`password Don't Match............`);
+                }
+                else{
+                    let token_id =  jwt.sign({ userid, userpassword},ACCESS_TOKEN,{expiresIn:'1d'});
 
-                        console.log(token_id);
+                    console.log(token_id);
 
-                                sessions[sessionId] = {userid,userpassword : 1};
+                        sessions[sessionId] = {userid,userpassword : 1};
 
-                                res.set('Set-Cookie',`session=${sessionId}`);
-                                res.status(200).json({
-                                'Result' :'Success...',
-                                'token' : token_id
-                                });  
-                        }
+                            res.set('Set-Cookie',`session=${sessionId}`);
+                            res.status(200).json({
+                            'Result' :'Success...',
+                            'token' : token_id
+                            });  
+                    }
                 
-            } catch (error) {
+            }catch(err) {
                 res.status(500).json({
-                    error: 'User NOT Register.....'});
+                    err: 'User NOT Register.....'});
             }
     },
     auth:async (req,res)=>{
@@ -85,49 +85,42 @@ const student ={
             
     lendBook:async(req,res)=>{
         try {
-            //console.log(req.user.userid);
 
             let id = req.user.userid;
             let query = req.query;
             console.log(id)
-    
-                // const result = await collection.find({"std_id":{$eq:id}}).countDocuments();
-    
-                // console.log(result);
 
-                // if(result===1){
+                    //find book list of lend Books...............
     
-                    console.log(req.query);
+            console.log(req.query);
 
-                    const value = await booklist.findOne(query);
-                    console.log(value);
-                    const copy=--value['Book_Copy'];
-                    console.log(value);
-                    await booklist.updateOne(query,{$set:{Book_Copy:copy}})
+            const value = await booklist.findOne(query);
+            console.log(value);
+            const copy=--value['Book_Copy'];
+            console.log(value);
+
+                    //update book list copys value............
+            await booklist.updateOne(query,{$set:{Book_Copy:copy}})
                     
-                    const book = {
+                const book = {
                         std_id : id,
                         Book_num :value.Book_num,
                         Book_Name : value.Book_Name,
                         Book_title : value.Book_title,
                         Book_Author : value.Book_Author
                     };
-                    // add = ++add
-                    // book['Book_Copy'] = add;
-                    // console.log(book['Book_Copy']);
-                    console.log(book);
-                     const result =await lendbook.create(book);
-                    console.log(result);
+                console.log(book);
+
+                    //lend Book stored methods................
+                const result =await lendbook.create(book);
+                console.log(result);
                     
-                res.send('Sucess.........');
+            res.send('Sucess.........');
 
-                // }else{
-                //     res.send(' User NOT found.........');
-                // }
             
-        } catch (error) {
+        }catch(err) {
 
-            res.status(500).send("Internal Server Error....");
+            res.status(500).send({'Internal Server Error....':err});
         }   
     },
     viwe_Book:async(req,res)=>{
@@ -137,9 +130,9 @@ const student ={
             let query = req.query;
             console.log(id)
     
-                const result = await booklist.find();
+            const result = await booklist.find();
     
-                console.log(result.length);
+            console.log(result.length);
                 if(result.length!==0){
 
                     res.send({'Result':result});
@@ -148,15 +141,13 @@ const student ={
                     res.send("No Book's _____ Pls Add Book....... ");
                 }
     
-        } catch (error) {
-
+        }catch(error) {
             res.status(500).send("User Error :"+error);
-            
         }
     },
     Return_Book:async(req,res)=>{
 
-            try {
+        try {
 
                 let id = req.user.userid;
                 let R_Book = req.query;
@@ -165,53 +156,52 @@ const student ={
 
                 console.log(R_Book);
                 console.log(booknum);
-
+                //find student lend Book's.........................
                 const s_one = await lendbook.findOne( {$and:[{date:{$eq:date}},{Book_num:{$eq:booknum}},{std_id:{$eq:id}}]})
                 console.log(s_one);
                 if(s_one!==null){
-                const D_book = {
+                    const D_book = {
 
-                    std_id : s_one.std_id,
-                    Book_num : s_one.Book_num,
-                    Book_Name : s_one.Book_Name,
-                    Book_title : s_one.Book_title,
-                    Book_Author : s_one.Book_Author
+                        std_id : s_one.std_id,
+                        Book_num : s_one.Book_num,
+                        Book_Name : s_one.Book_Name,
+                        Book_title : s_one.Book_title,
+                        Book_Author : s_one.Book_Author
+                    }
+                        //student stored return Books...........
+                    await deletebook.create(D_book);
+
+                        //lend Book Delete methods.................. 
+                    const d_one = await lendbook.deleteOne( {$and:[{date:{$eq:date}},{Book_num:{$eq:booknum}},{std_id:{$eq:id}}]})
+
+                    console.log(d_one.deletedCount);
+                        if(d_one.deletedCount===1){
+                            //update lend book copys methods.........
+
+                            const value = await booklist.findOne(R_Book);
+                            console.log(value);
+                            const copy1=++value['Book_Copy'];
+                            console.log(value);
+
+                                //update value insert methods....................
+                            await booklist.updateOne(R_Book,{$set:{Book_Copy:copy1}});
+
+                            res.send({'Success':value});
+                        }else{
+                            res.send({'Error':'You did not pick up this book......'})
+                        }
+                }else{
+                    console.log("You did not pick up this book..........");
+                
+                    res.send("You did not pick up this book.................")
                 }
+
+        } catch (Error) {
+            console.log('Error : '+Error);
                 
-                await deletebook.create(D_book);
+            res.send({'user Error':Error});
+        }
 
-                //lend Book Delete methods.................. 
-                const d_one = await lendbook.deleteOne( {$and:[{date:{$eq:date}},{Book_num:{$eq:booknum}},{std_id:{$eq:id}}]})
-
-               console.log(d_one.deletedCount);
-               if(d_one.deletedCount===1){
-                //update lend book copys methods.........
-
-                const value = await booklist.findOne(R_Book);
-                console.log(value);
-                const copy1=++value['Book_Copy'];
-                console.log(value);
-
-                //update value insert methods....................
-                await booklist.updateOne(R_Book,{$set:{Book_Copy:copy1}});
-
-               
-                res.send({'Success':value});
-               }else{
-                res.send({'Error':'You did not pick up this book......'})
-               }
-            }else{
-                console.log("You did not pick up this book..........");
-                
-                res.send("You did not pick up this book.................")
-            }
-
-            } catch (Error) {
-                console.log('Error : '+Error);
-                
-                res.send({'user Error':Error});
-
-            }
     }
 };
 
